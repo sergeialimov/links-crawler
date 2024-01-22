948fe9b5609aa0b2d2ceffc5fe2848e06e183131
Sergei <dev.sergeialimov@gmail.com>
2024-01-22 16:44:10 +0300

    add yahoo crawler

---
+++ crawlers/yahooCrawler.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+async function crawlYahoo(keyword, page) {
+  // Launch the browser
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+
+  // Navigate to the Yahoo search page
+  await page.goto(`https://search.yahoo.com/search?p=${encodeURIComponent(keyword)}&b=${page}`);
+
+  // Wait for the ads to load using the selectors you provided
+  await page.waitForSelector('.searchCenterBottomAds');
+  await page.waitForSelector('.searchCenterTopAds');
+
+  // Extract the links from the ads
+  const links = await page.evaluate(() => {
+    const bottomAdsLinks = Array.from(document.querySelectorAll('.searchCenterBottomAds a')).map(a => a.href);
+    const topAdsLinks = Array.from(document.querySelectorAll('.searchCenterTopAds a')).map(a => a.href);
+    return bottomAdsLinks.concat(topAdsLinks);
+  });
+
+  // Close the browser
+  await browser.close();
+
+  // Return the links
+  return links;
+}
+
+module.exports = { crawlYahoo };
