cf7f5ff92a926def8519a067fe3ef6006081c982
Sergei <dev.sergeialimov@gmail.com>
2024-01-22 19:17:54 +0300

    handle missing params

--- routes/sponsoredLinksRoutes.js
+++ routes/sponsoredLinksRoutes.js
@@ -4,11 +4,17 @@ function apiRoutes(app) {
   app.get('/api/v1/sponsored-links', async (req, res) => {
     try {
       const { pages, keywords } = req.query;
+
+      if (!pages || !keywords) {
+        return res.status(400).send('You must provide both pages and keywords parameters');
+      }
+
       const keywordsArray = keywords.split(',');
+
       const results = await startCrawling(pages, keywordsArray);
-      res.json(results);
+      return res.json(results);
     } catch (error) {
-      res.status(500).send(error.message);
+      return res.status(500).send(error.message);
     }
   });
 }
