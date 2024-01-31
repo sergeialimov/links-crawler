f44e755844bd61b0e6e5be3fc465b110f12f435b
Sergei <dev.sergeialimov@gmail.com>
2024-01-31 17:12:32 +0400

    feat: add error handling to yahoo crawler

--- crawlers/yahooCrawler.js
+++ crawlers/yahooCrawler.js
@@ -6,32 +6,42 @@ const {
 } = require('../constants');
 
 async function crawlYahoo(keyword, pageNumber) {
-  const browser = await puppeteer.launch({ headless: process.env.HEADLESS_MODE });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ headless: process.env.HEADLESS_MODE });
+    const page = await browser.newPage();
 
-  const url = `${YAHOO_BASE_URL}?p=${encodeURIComponent(keyword)}&fp=${pageNumber}`;
-  await page.goto(url);
+    const url = `${YAHOO_BASE_URL}?p=${encodeURIComponent(keyword)}&fp=${pageNumber}`;
+    await page.goto(url);
 
-  const sponsoredLinks = await page.evaluate((selectors) => {
-    const adElements = document.querySelectorAll(selectors);
-    const links = [];
-    adElements.forEach((element) => {
-      const href = element.getAttribute('href');
-      if (href) {
-        links.push(href);
-      }
-    });
-    return links;
-  }, YAHOO_AD_SELECTORS);
+    const sponsoredLinks = await page.evaluate((selectors) => {
+      const adElements = document.querySelectorAll(selectors);
+      const links = [];
+      adElements.forEach((element) => {
+        const href = element.getAttribute('href');
+        if (href) {
+          links.push(href);
+        }
+      });
+      return links;
+    }, YAHOO_AD_SELECTORS);
 
-  await browser.close();
+    await browser.close();
 
-  return {
-    searchEngine: SEARCH_ENGINES.YAHOO,
-    keyword,
-    sponsoredLinks,
-    pageNum: pageNumber,
-  };
+    return {
+      searchEngine: SEARCH_ENGINES.YAHOO,
+      keyword,
+      sponsoredLinks,
+      pageNum: pageNumber,
+    };
+  } catch (error) {
+    console.error(`Error occurred while crawling Yahoo: ${error}`);
+    return {
+      searchEngine: SEARCH_ENGINES.YAHOO,
+      keyword,
+      sponsoredLinks: [],
+      pageNum: pageNumber,
+    };
+  }
 }
 
 module.exports = { crawlYahoo };
