05889b51b654266f3ccb27005efe202ccf1d6743
Sergei <dev.sergeialimov@gmail.com>
2024-01-31 17:19:57 +0400

    refactor: move search engines to env

--- workers/crawlManager.js
+++ workers/crawlManager.js
@@ -25,11 +25,7 @@ function runService(workerData) {
 }
 
 async function startCrawling(pages, keywords) {
-  const searchEngines = [
-    'google',
-    'yahoo',
-    'bing',
-  ];
+  const searchEngines = process.env.SEARCH_ENGINES.split(',');
 
   // Create an array of tasks using array methods instead of loops
   const tasks = keywords.flatMap((keyword) => searchEngines.flatMap((searchEngine) => Array
